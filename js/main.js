e5fc5cf688b4262da8bf8b62c4a8fb11c76df899
Fabricio <jfguardiacpw@gmail.com>
2020-02-10 13:16:58 -0800

    Refactoring setting card valuess code

--- js/main.js
+++ js/main.js
@@ -40,10 +40,12 @@ let tableMoneyDown = 0;
 let cardsGenerated = [];
 let dealerHiddenCard;
 let count = [];
+let unavailableCards = [];
 
 /* ----- CACHED ELEMENT REFERENCE ----*/
 let budget = document.querySelector("#budget");
 let beforeWager = document.querySelector("#beforeWager");
+let range = document.querySelector('#range');
 let total = document.querySelector("#total");
 let totalBet = document.querySelector("#totalBet");
 let doubleDown = document.querySelector("#doubleDown");
@@ -59,17 +61,26 @@ let dealerCards = document.querySelectorAll('#dealerCards > img');
 let playerCards = document.querySelectorAll('#playerCards > img');
 let dealerCountTotal = document.querySelector('#dealerCount');
 let playerCountTotal = document.querySelector('#playerCount');
+let newCard = document.createElement('img');
 
 /* ----- EVENT LISTENERS ----*/
 beforeWager.addEventListener('click', buyChips);
 readyBet.addEventListener('click', placeBet);
 TABLE.moves.deal.addEventListener('click', deal);
+budget.addEventListener('keyup', function(e){
+    if(e.keyCode === 13){
+        e.preventDefault();
+        beforeWager.click();
+    }
+})
+TABLE.moves.hit.addEventListener('click', hit);
 
 /* ----- FUNCTIONS ----*/
 
 init();
 
 function init(){
+    budget.focus();
     render();    
 }
 
@@ -78,13 +89,32 @@ function render(){
     document.querySelector("#table").style.cssText = "background-color:#3A7B28!important; box-shadow: 0 0 200px rgba(0,0,0,0.9) inset; height:85vmin; width:100%; border-radius:10px; margin-top:6vmin;";
     instructions.innerHTML = "Set your bankroll...";
     $(instructions).slideDown(600);
+    $(range).slideDown(1500);
     for(let moves in TABLE.moves){
         TABLE.moves[moves].style.cssText = "display:none;";
     }
-
+    
     // for(let images in TABLE.bets){
     //     readyBet = TABLE.bets[images];
     // }
+}
+
+function hit(){
+    // Check if flip is black jack or hit
+    let cardType = Math.ceil(Math.random()*13);
+    count.push(cardType);
+    let suit = Math.floor(Math.random()*TABLE.suits.length);
+    let newCard = generateCard(cardType, suit);
+    cardsGenerated.push(newCard);
+    unavailableCards.push(newCard);
+    document.querySelector('#playerCards').appendChild(newCard);
+    let newPlayerCards = document.querySelectorAll('#playerCards > img');
+    newPlayerCards[2].src = `images/${cardsGenerated[0]}.png`;
+    $(newPlayerCards).fadeIn(1500);
+
+    // dealerCards[0].src = `images/${cardsGenerated[0]}.png`;
+    // dealerCountTotal.innerHTML = count[0] + count[1];
+    console.log(count[0] + count[1]);
 }
 
 function deal(e){
@@ -100,15 +130,14 @@ function deal(e){
         $(TABLE.bets[images].firstElementChild).click(false);
     }
 
-    // Create array and file name for each randomly generated cards for dealer 
-
+    // Create array and define file directory for each randomly generated cards for dealer and player
     for(let i = 0; i < 4; i++){
-        let cardType = Math.ceil(Math.random()*13);
-        count.push(cardType);
-        let suit = Math.floor(Math.random()*TABLE.suits.length);
-        let newCard = generateCard(cardType, suit);
-        cardsGenerated.push(newCard);
+        randomCards();
     }
+
+    PLAYERS.playerHands.hands += count[2]+count[3];
+    console.log(PLAYERS.playerHands.hands);
+    console.log(count);
     
     // Arrange table for dealer with random selected cards
     setTimeout(function(){
@@ -122,6 +151,8 @@ function deal(e){
         $(dealerCards).fadeIn(1500);
     }, 200);
 
+    count.splice(0,2);
+    console.log(count);
     
     // Arrange table for player with random selected cards
     setTimeout(function(){
@@ -134,27 +165,23 @@ function deal(e){
         $(playerCards).fadeIn(1500);
     }, 200);
 
+    count.splice(0,2);
+
     // Setting value of cards based on cards selected
 
-    count[0] === 11 || count[0] === 12 || count[0] === 13 ? count[0] = 10 : count[0];
-    count[1] === 11 || count[1] === 12 || count[1] === 13 ? count[1] = 10 : count[1];
-    if(count[0] === 1){count[0] + count[1] > 21 ? count[0] = 1: count[0] = 11;}
-    if(count[1] === 1){count[0] + count[1] > 21 ? count[1] = 1: count[1] = 11;}
+    setValues();
+
+    PLAYERS.playerHands.hands += count[2]+count[3];
+    PLAYERS.dealer.hands += count[0]+count[1];
 
     dealerCountTotal.innerHTML = count[1];
     $(dealerCountTotal).fadeIn(2000);
 
-    count[2] === 11 || count[2] === 12 || count[2] === 13 ? count[2] = 10 : count[2];
-    count[3] === 11 || count[3] === 12 || count[3] === 13 ? count[3] = 10 : count[3];
-    if(count[2] === 1){count[2] + count[3] > 21 ? count[2] = 1: count[2] = 11;}
-    if(count[3] === 1){count[2] + count[3] > 21 ? count[3] = 1: count[3] = 11;}
-
     playerCountTotal.innerHTML = count[2]+count[3];
     $(playerCountTotal).fadeIn(2000);
 
-    // Setting up blackjack rules for dealer flip
+    generateCard = [];
 
-    
 }
 
 function generateCard(a, b){
@@ -166,6 +193,32 @@ function generateCard(a, b){
     if(a === 13){cardType = 'k'};
     let cardSelected = `${cardType}${suit}`;
     return cardSelected;
+}
+
+function randomCards(){
+    let cardType = Math.ceil(Math.random()*13);
+    count.push(cardType);
+    let suit = Math.floor(Math.random()*TABLE.suits.length);
+    let newCard = generateCard(cardType, suit);
+    cardsGenerated.push(newCard);
+    unavailableCards.push(newCard);
+}
+
+function setValues(){
+    let faceValues = [11, 12, 13];
+    for(let i = 0; i < faceValues.length; i++){
+        
+    }
+    count[0] === 11 || count[0] === 12 || count[0] === 13 ? count[0] = 10 : count[0];
+    count[1] === 11 || count[1] === 12 || count[1] === 13 ? count[1] = 10 : count[1];
+    if(count[0] === 1){count[0] + count[1] > 21 ? count[0] = 1: count[0] = 11;}
+    if(count[1] === 1){count[0] + count[1] > 21 ? count[1] = 1: count[1] = 11;}
+
+    count[2] === 11 || count[2] === 12 || count[2] === 13 ? count[2] = 10 : count[2];
+    count[3] === 11 || count[3] === 12 || count[3] === 13 ? count[3] = 10 : count[3];
+    if(count[2] === 1){count[2] + count[3] > 21 ? count[2] = 1: count[2] = 11;}
+    if(count[3] === 1){count[2] + count[3] > 21 ? count[3] = 1: count[3] = 11;}
+    
 }
 
 function placeBet(e){
@@ -214,6 +267,7 @@ function buyChips(e){
     let moneyDown = PLAYERS.playerHands.moneyDown = budget.value;
     budget.style.cssText = "display:none";
     beforeWager.style.cssText = "display:none";
+    range.style.cssText = "display:none";
     tableMoneyDown = Math.ceil((Math.round(moneyDown/10))*10);
     total.innerHTML = tableMoneyDown;
     total.style.cssText = "color: white; font-size:40px; padding:0 2vmin 0 3vmin;";
